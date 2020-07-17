06db5344ace33070c50ef9c8357b7ef37256e71e
Almissbah <mohammed.almissbah@hotmail.com>
2020-07-17 15:20:12 +0400

    Add Article to/from json conversion.

--- app/src/main/java/com/almissbah/articles/data/remote/model/Article.kt
+++ app/src/main/java/com/almissbah/articles/data/remote/model/Article.kt
@@ -1,5 +1,6 @@
 package com.almissbah.articles.data.remote.model
 
+import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 
 data class Article(
@@ -8,4 +9,14 @@ data class Article(
     val byline: String,
     val abstract: String,
     @SerializedName("published_date") val publishedDate: String
-)
+) {
+    companion object {
+        fun fromString(string: String): Article? {
+            return Gson().fromJson(string, Article::class.java)
+        }
+    }
+
+    override fun toString(): String {
+        return Gson().toJson(this).toString()
+    }
+}
